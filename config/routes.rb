d778c8ac9495e0947024b1c15b8dbd71a8789477
your github.com username <kandeel.a1@gmail.com>
2017-02-26 22:47:12 -0500

    Refatoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pictures#index'
-  get 'pictures' => 'pictures#index'
-
+  # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  # root 'pictures#index'
+  # get 'pictures' => 'pictures#index'
+  #
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # patch 'pictures/:id' => "pictures#update"
   # post 'pictures' => 'pictures#create' # this is a new line of code
   # get 'pictures/new' => 'pictures#new' # this is also a new line of code
   #
   # get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  root 'pictures#index'
+
+ resources :pictures
+
 
 end
